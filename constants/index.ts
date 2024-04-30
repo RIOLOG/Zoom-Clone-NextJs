3d7e0bbbe6ff5080d9c437d1f89efe77ba54750c
Ankit <ankit.singh14@nagarro.com>
2024-05-01 00:30:54 +0530

    Zoom App Next Js

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+    {
+        imageUrl:'/icons/Home.svg',
+        route:'/',
+        label:'Home'
+    },
+    {
+        imageUrl:'/icons/upcoming.svg',
+        route:'/upcoming',
+        label:'Upcoming'
+    },
+    {
+        imageUrl:'/icons/previous.svg',
+        route:'/previous',
+        label:'Previous'
+    },
+    {
+        imageUrl:'/icons/recordings.svg',
+        route:'/recording',
+        label:'Recording'
+    },
+    {
+        imageUrl:'/icons/add-personal.svg',
+        route:'/personal-room',
+        label:'Personal Room'
+    }
+]
