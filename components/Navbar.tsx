9a73a0a23b4667cf082cbf7c11797716ebdbd5bf
Ankit <ankit.singh14@nagarro.com>
2024-05-03 23:32:48 +0530

    Zopm App NextJs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { UserButton, SignedIn } from '@clerk/nextjs'
-// import MobileNav from './MobileNav'
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
-    return (
-        <nav className='fkex-between fixed z-50 w-full 
-        bg-dark-1 px-6 py-4 lg:px-10'>
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          YOOM
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
 
-            <Link href="/" className="flex items-center gap-1">
-                <Image 
-                    src="/icons/logo.svg"
-                    width={32}
-                    height={32}
-                    alt="Zoom Room"
-                    className="max-sm:size-10"
-                />
-                <p className="text-[26px] font-extrabold text-white max-sm:hidden"></p>
-            </Link>
-
-
-            <div className='flex-between gap-5'>
-
-                <SignedIn>
-                    <UserButton/>
-                </SignedIn>
-
-                {/* <SignedOut>
-                    <SignedIn></SignedIn>
-                </SignedOut> */}
-
-                {/* <MobileNav/> */}
-
-            </div>
-
-
-        </nav>
-    )
-}
-
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
